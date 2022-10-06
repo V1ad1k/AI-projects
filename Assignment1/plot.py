589ae8027272ba0750d2b7880da154cc093b1dc2
Vladyslav Gavryliuk <58902929+V1ad1k@users.noreply.github.com>
2022-10-06 18:20:47 +0200

    Add files via upload

---
+++ Assignment1/plot.py
@@ -0,0 +1,28 @@
+# import matplotlib.pyplot as plt
+  
+# y1 = [13, 18, 17 ,20, 26, 37 ,47, 43, 58 ,122 ,146]
+# y2= [5,	14 ,15,	17,	24, 29, 35, 38,	59,	107, 134]
+# y3 = [1,4,12,16,23,27,32,34,36,43,55]
+
+# y4 = [21,48,61,119,368,833,1888,3307,4517,6403,10410]
+# y5=  [6,21,53,97,341,496,1023,2518,4128,5619,6567]
+# y6 = [1,6,15,22,30,44,49,50,56,51,57]
+
+
+# x = [2,3,4,5,6,7,8,9,10,11,12]
+  
+# # plotting the points 
+
+# plt.plot(x, y4, label = "Backtracking")
+# plt.plot(x, y5, label = "Forwardchecking")
+# plt.plot(x, y6, label = "AC")
+# plt.xlabel('Size')
+# plt.ylabel('Domains')
+# # giving a title to my graph
+# # plt.title('My first graph!')
+# plt.legend()
+# # function to show the plot
+# plt.show()
+
+a = {'b':'a','d':'d'}
+print(list(a.keys()))
